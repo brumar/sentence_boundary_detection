3faf384e45eb6337cb041e88036659bbfd455a00
Bruno <martin.bruno.mail@gmail.com>
2020-01-08 08:44:14 +0100

    exploration of brown corpus

--- sdb/utils.py
+++ sdb/utils.py
@@ -1,16 +1,23 @@
-from typing import List
+from typing import List, Iterable
 from itertools import islice
+from pathlib import Path
+
+file_path = Path(__file__)
+project_directory = file_path.parent.parent
+
+DATA_DIR = project_directory / Path("data")
 
 
 def read_segmented_corpus_iter(corpus):
-    with open(f"./data/{corpus}/segmented.txt", "r") as f:
+    filepath = DATA_DIR / Path(corpus) / "segmented.txt"
+    with open(filepath, "r") as f:
         line = f.readline()
         while line:
             yield line
             line = f.readline()
     
 
-def raw_surroundings(lines: List[str])->List[tuple]:
+def raw_surroundings(lines: Iterable[str])->List[tuple]:
     """capture last two cars and next two cars around
     terminaison point
     """
