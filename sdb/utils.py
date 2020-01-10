25dd1df2acfb97dc56dc41e27264d639efe99323
Bruno <martin.bruno.mail@gmail.com>
2020-01-10 07:26:02 +0100

    tooling to select and evaluate candidates

--- sdb/utils.py
+++ sdb/utils.py
@@ -1,5 +1,4 @@
 from typing import List, Iterable
-from itertools import islice
 from pathlib import Path
 
 file_path = Path(__file__)
@@ -7,22 +6,134 @@ project_directory = file_path.parent.parent
 
 DATA_DIR = project_directory / Path("data")
 
+CANDIDATES = ""
+NORMAL_CANDIDATES = set([";", ":", ".", "!", "?"])
 
-def read_segmented_corpus_iter(corpus):
-    filepath = DATA_DIR / Path(corpus) / "segmented.txt"
+
+def read_unsegmented_corpus_iter(corpus: str):
+    target = "unsegmented.txt"
+    yield from _read_corpus(corpus, target)
+
+
+def read_segmented_corpus_iter(corpus: str):
+    target = "segmented.txt"
+    yield from _read_corpus(corpus, target)
+
+
+def _read_corpus(corpus, target):
+    filepath = DATA_DIR / Path(corpus) / target
     with open(filepath, "r") as f:
         line = f.readline()
         while line:
             yield line
             line = f.readline()
-    
 
-def raw_surroundings(lines: Iterable[str])->List[tuple]:
+
+def split_at_all_candidates(input_string_unsegmented: List[str]):
+    # tech debt warning
+    # this behavior is tightly coupled with is_gold_findable
+    # maintain both functions together
+    ignore_next_car = False
+    for line in input_string_unsegmented.splitlines():
+        current_line = []
+        for index_car, car in enumerate(line):
+            if ignore_next_car:
+                ignore_next_car = False
+                continue
+            current_line.append(car)
+            if ((car == ".") and (index_car != len(line) - 1)) and (
+                line[index_car + 1]
+            ) == '"':
+                current_line.append('"')
+                # print("---------------")
+                # print("".join(current_line))
+                yield "".join(current_line)
+                current_line = []
+                ignore_next_car = True
+            elif car in NORMAL_CANDIDATES:
+                yield "".join(current_line)
+                current_line = []
+        if current_line:
+            yield "".join(current_line)
+
+
+def is_gold_findable(gold: str):
+    # tech debt warning
+    # this behavior is tightly coupled with split_at_all_candidates
+    # maintain both functions together
+    if gold.endswith('."'):
+        return True
+    if gold.endswith("\n"):
+        return True
+    if any((gold.endswith(x) for x in NORMAL_CANDIDATES)):
+        return True
+    return False
+
+
+def evaluate_candidates(
+    candidates: Iterable[str], list_result: Iterable[str]
+) -> Iterable[bool]:
+    """returns an iterable giving booleans evaluating if candidates match"""
+    gold_iterator = iter(list_result)
+    advance_gold_iterator = True
+    buffer = ""
+    for c, cand in enumerate(candidates):
+        if not cand or cand.isspace():
+            continue
+        if advance_gold_iterator:
+            current_gold = advance_to_next_findable_gold(gold_iterator)
+        buffer = buffer + cand
+
+        # print("cand->",cand)
+        # print("gold->", current_gold)
+        # print("buff->",buffer)
+        # print("--")
+        if len(buffer) > 2000 or len(current_gold) > 2000:
+            raise Exception
+
+        # important : candidates may have extra white spaces or linebreaks
+        # this is useful information for model training but they should be
+        # stripped off to be properly be compared to gold standards which do
+        # not include this information
+
+        # as buffer and current gold can be the result of a concatenation with
+        # uncontrolled whitespaces, we are better off
+        # stripping all the whitespaces
+        # on both string
+        # as gold iterable and buffer iterable advance together,
+        # the chances of false positive are unlikely
+        # print(cand, current_gold)
+        # import pdb; pdb.set_trace()
+        if buffer.replace(" ", "") == current_gold.replace(" ", ""):
+            buffer = ""
+            yield True
+            advance_gold_iterator = True
+        else:
+            yield False
+            advance_gold_iterator = False
+
+
+def advance_to_next_findable_gold(gold_iterator) -> str:
+    try:
+        current_gold = next(gold_iterator).rstrip()
+        while not is_gold_findable(current_gold):
+            current_gold = current_gold + next(gold_iterator).rstrip()
+    except StopIteration:
+        raise Exception(
+            """Bug. the target sentences have all been passed but candidates remain.
+                        This is likely an error in is_gold_findable function"""
+        )
+    return current_gold
+
+
+def raw_surroundings(lines: Iterable[str]) -> List[tuple]:
     """capture last two cars and next two cars around
     terminaison point
     """
-    for l, l2 in zip(islice(lines, 0, None), islice(lines, 1, None)):
-        if len(l) > 2 and len(l2) > 1:
-            last_letters_no_breaking_line = l[-3:-1]
-            first_new_letters = l2[:2]
+    previous_line = None
+    for l in lines:
+        if previous_line is not None:
+            last_letters_no_breaking_line = previous_line[-3:-1]
+            first_new_letters = l[:2]
             yield (last_letters_no_breaking_line, first_new_letters)
+        previous_line = l
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,37 @@
 import sys
 from pathlib import Path
+import statistics
+from sdb import utils
+import pytest
 
 file_path = Path(__file__)
 project_directory = file_path.parent.parent
 sys.path.append(str(project_directory))
 # DIRTY TRICK SO THAT sdb module can be found by manipulating PYTHONPATH
 
-
-from sdb import utils
-import pytest
+INPUT_STRING_UNSEGMENTED = "".join(
+    [
+        "candidates can have various forms! Why using only punctuations?",
+        (
+            "Observations on corpus seems to find that ; and : "
+            "can be potential candidates too! There is also"
+        ),
+        (
+            ' a special case, the "special case." Here the'
+            "real termination point should be the quote."
+        ),
+    ]
+)
+LIST_SEGMENTED = [
+    "candidates can have various forms!",
+    "Why using only punctuations?",
+    (
+        "Observations on corpus seems to find "
+        "that ; and : can be potential candidates too!"
+    ),
+    'There is also a special case, the "special case."',
+    'Here the real termination point should be the quote."',
+]
 
 
 @pytest.fixture
@@ -22,3 +45,35 @@ def test_view_surroundings_end_sentences(ten_sentences_brown):
     assert len(surroundings) == 9, "no surroundings for last sentence"
     assert tuple(surroundings[0]) == ("e.", "Th")
     assert tuple(surroundings[1]) == ("d.", "Th")
+
+
+def test_get_all_candidates():
+
+    result = list(utils.split_at_all_candidates(INPUT_STRING_UNSEGMENTED))
+    assert len(result) == 7
+    assert result[0] == "candidates can have various forms!"
+    assert result[-2].strip() == ('There is also a special case, the "special case."')
+
+
+def test_evaluate_candidates():
+
+    result = list(utils.split_at_all_candidates(INPUT_STRING_UNSEGMENTED))
+
+    boolean_evals = list(utils.evaluate_candidates(result, LIST_SEGMENTED))
+
+    assert boolean_evals[0] is True
+    assert sum(boolean_evals) == 4
+
+
+def test_evaluate_candidates_on_real_data():
+    for corpus in ["wsj", "brown"]:
+        unsegmented_string = "".join(utils.read_unsegmented_corpus_iter(corpus))
+        candidates_iterable = utils.split_at_all_candidates(unsegmented_string)
+        segmented_iterable = utils.read_segmented_corpus_iter(corpus)
+
+        booleans_iterable = utils.evaluate_candidates(
+            candidates_iterable, segmented_iterable
+        )
+
+        m = statistics.mean(booleans_iterable)
+        assert m > 0.5
